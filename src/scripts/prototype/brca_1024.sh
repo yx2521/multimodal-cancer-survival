8372da82038e086fc78b4d54c129e5beaf96c03a
Yilan Xu <yx423@gpu-q-21.data.cluster>
2025-06-10 16:49:35 +0100

    Add MMP core code

---
+++ src/scripts/prototype/brca_1024.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gpuid=$1
+
+declare -a dataroots=(
+#	'path/to/tcga_brca'
+	'/home/yx423/rds/hpc-work/MMP_Slide_Embed/tcga_brca'
+)
+
+# Loop through different folds
+for k in 0 1 2 3 4; do
+	split_dir="survival/TCGA_BRCA_overall_survival_k=${k}"
+	split_names="train"
+	bash "./scripts/prototype/clustering_brca_1024.sh" $gpuid $split_dir $split_names "${dataroots[@]}"
+done
---
+++ src/scripts/prototype/stad_1024.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gpuid=$1
+
+declare -a dataroots=(
+#	'path/to/tcga_stad'
+	'/home/yx423/rds/hpc-work/MMP_Slide_Embed/tcga_stad'
+)
+
+# Loop through different folds
+for k in 0 1 2 3 4; do
+	split_dir="survival/TCGA_STAD_overall_survival_k=${k}"
+	split_names="train"
+	bash "./scripts/prototype/clustering_stad_1024.sh" $gpuid $split_dir $split_names "${dataroots[@]}"
+done
