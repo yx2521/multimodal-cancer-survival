5b8a40522bbd4b856bd277608f967e506972af05
Yilan Xu <yx423@login-q-1.data.cluster>
2025-07-06 17:10:09 +0100

    Update key scripts

---
+++ src/scripts/prototype/2048/kirc_2048.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gpuid=$1
+
+declare -a dataroots=(
+#	'path/to/tcga_kirc'
+	'/home/yx423/rds/hpc-work/MMP_Slide_Embed/tcga_rcc'
+)
+
+# Loop through different folds
+for k in 0 1 2 3 4; do
+	split_dir="survival/TCGA_KIRC_overall_survival_k=${k}"
+	split_names="train"
+	bash "./scripts/prototype/2048/clustering_kirc_2048.sh" $gpuid $split_dir $split_names "${dataroots[@]}"
+done
